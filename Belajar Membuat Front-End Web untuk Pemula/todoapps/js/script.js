ddb7c74daeb15ce8ba9d569215524cdc65aa6582
bimadharma <bimadharmayahya@gmail.com>
2024-10-15 21:28:11 +0700

    storage

--- Belajar Membuat Front-End Web untuk Pemula/todoapps/js/script.js
+++ Belajar Membuat Front-End Web untuk Pemula/todoapps/js/script.js
@@ -87,14 +87,17 @@ document.addEventListener(RENDER_EVENT, function () {
     const uncompletedTODOList = document.getElementById('todos');
     uncompletedTODOList.innerHTML = '';
    
+    const completedTODOList = document.getElementById('completed-todos');
+    completedTODOList.innerHTML = '';
+   
     for (const todoItem of todos) {
       const todoElement = makeTodo(todoItem);
-      if (!todoItem.isCompleted) {
+      if (!todoItem.isCompleted)
         uncompletedTODOList.append(todoElement);
-      }
+      else
+        completedTODOList.append(todoElement);
     }
   });
-
 
   function addTaskToCompleted (todoId) {
     const todoTarget = findTodo(todoId);
@@ -112,4 +115,33 @@ document.addEventListener(RENDER_EVENT, function () {
       }
     }
     return null;
+  }
+
+  function removeTaskFromCompleted(todoId) {
+    const todoTarget = findTodoIndex(todoId);
+   
+    if (todoTarget === -1) return;
+   
+    todos.splice(todoTarget, 1);
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+   
+   
+  function undoTaskFromCompleted(todoId) {
+    const todoTarget = findTodo(todoId);
+   
+    if (todoTarget == null) return;
+   
+    todoTarget.isCompleted = false;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  function findTodoIndex(todoId) {
+    for (const index in todos) {
+      if (todos[index].id === todoId) {
+        return index;
+      }
+    }
+   
+    return -1;
   }
